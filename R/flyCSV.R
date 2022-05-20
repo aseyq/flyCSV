311df7b4e835483b977b320cd19fd236e3b37be5
aseyq <aseyq@posteo.net>
2022-05-20 17:15:32 +0200

    corrected example function description

--- R/flyCSV.R
+++ R/flyCSV.R
@@ -1,4 +1,4 @@
-#' Load a Matrix
+#' Open your data as CSV
 #'
 #' This function loads a file as a matrix. It assumes that the first column
 #' contains the rownames and the subsequent columns are the sample identifiers.
