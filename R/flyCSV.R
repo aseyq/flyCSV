a0b22745815f4cf016dd3a812ac8be91d1ddda9f
Ali Seyhun Saral <82848414+aseyq@users.noreply.github.com>
2022-05-21 15:16:47 +0200

    Update flyCSV.R

--- R/flyCSV.R
+++ R/flyCSV.R
@@ -11,15 +11,16 @@
 #' @param browser a non-empty character string giving the name of the program
 #' @param rownames creates a row name column at the beginnig. Usually rows are named as the line number
 #'                to be used as the HTML browser.
+#' @param encoding File encoding
 #' @return df Returns the original object
 #' @export
-flyCSV <- function(df, filename=NA, browser=getOption("browser"), row.names=FALSE) {
+flyCSV <- function(df, filename=NA, browser=getOption("browser"), row.names=FALSE, encoding = "") {
     
     if (is.na(filename)) {
         filename <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = ".csv")
     }
     
-    write.csv(df, filename, row.names=row.names)
+    write.csv(df, filename, row.names=row.names, fileEncoding=encoding)
     browseURL(filename, browser=browser)    
     invisible(df)
 }
